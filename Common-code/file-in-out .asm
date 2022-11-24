82a21aed5141f9d95c063be287ce7ed90bf4d3f0
mina ilkhani <83788223+minailkhani@users.noreply.github.com>
2022-11-24 15:44:00 +0330

    Add files via upload

---
+++ Common-code/file-in-out .asm
@@ -0,0 +1,157 @@
+%include "../general/sys-equal.asm"
+;----------------------------------------------------
+section     .data1
+
+    error_create    db     "error in creating file       ", NewLine, 0
+    error_close     db     "error in closing file        ", NewLine, 0
+    error_write     db     "error in writing file        ", NewLine, 0
+    error_open      db     "error in opening file        ", NewLine, 0
+    error_append    db     "error in appending file      ", NewLine, 0
+    error_delete    db     "error in deleting file       ", NewLine, 0
+    error_read      db     "error in reading file        ", NewLine, 0
+    error_print     db     "error in printing file       ", NewLine, 0
+    error_seek      db     "error in seeking file        ", NewLine, 0
+
+    suces_create   db      "file created and opened for R/W ", NewLine, 0
+    suces_close    db      "file closed                     ", NewLine, 0
+    suces_write    db      "written to file                 ", NewLine, 0
+    suces_open     db      "file opend for R/W              ", NewLine, 0
+    suces_append   db      "file opened for appending       ", NewLine, 0
+    suces_delete   db      "file deleted                    ", NewLine, 0
+    suces_read     db      "reading file                    ", NewLine, 0
+    suces_seek     db      "seeking file                    ", NewLine, 0
+
+;----------------------------------------------------
+; rdi : file name; rsi : file permission
+createFile:
+    mov     rax, sys_create
+    mov     rsi, sys_IRUSR | sys_IWUSR 
+    syscall
+    cmp     rax, -1   ; file descriptor in rax
+    jle     createerror
+    mov     rdi, suces_create           
+    call    printString
+    ret
+createerror:
+    mov     rdi, error_create
+    call    printString
+    ret
+
+createFile:
+    mov     rax, sys_create
+    mov     rsi, sys_IRUSR | sys_IWUSR 
+    syscall
+    cmp     rax, -1   ; file descriptor in rax
+    jle     createerror
+    mov     rdi, suces_create           
+    call    printString
+    ret
+createerror:
+    mov     rdi, error_create
+    call    printString
+    ret
+;----------------------------------------------------
+; rdi : file name; rsi : file access mode 
+; rdx: file permission, do not need
+openFile:
+    mov     rax, sys_open
+    mov     rsi, O_RDWR     
+    syscall
+    cmp     rax, -1   ; file descriptor in rax
+    jle     openerror
+    mov     rdi, suces_open
+    call    printString
+    ret
+openerror:
+    mov     rdi, error_open
+    call    printString
+    ret
+;----------------------------------------------------
+; rdi point to file name
+appendFile:
+    mov     rax, sys_open
+    mov     rsi, O_RDWR | O_APPEND
+    syscall
+    cmp     rax, -1     ; file descriptor in rax
+    jle     appenderror
+    mov     rdi, suces_append
+    call    printString
+    ret
+appenderror:
+    mov     rdi, error_append
+    call    printString
+    ret
+;----------------------------------------------------
+; rdi : file descriptor ; rsi : buffer ; rdx : length
+writeFile:
+    mov     rax, sys_write
+    syscall
+    cmp     rax, -1         ; number of written byte
+    jle     writeerror
+    mov     rdi, suces_write
+    call    printString
+    ret
+writeerror:
+    mov     rdi, error_write
+    call    printString
+    ret
+;----------------------------------------------------
+; rdi : file descriptor ; rsi : buffer ; rdx : length
+readFile:
+    mov     rax, sys_read
+    syscall
+    cmp     rax, -1           ; number of read byte
+    jle     readerror
+    mov     byte [rsi+rax], 0 ; add a  zero
+    mov     rdi, suces_read
+    call    printString
+    ret
+readerror:
+    mov     rdi, error_read
+    call    printString
+    ret
+;----------------------------------------------------
+; rdi : file descriptor
+closeFile:
+    mov     rax, sys_close
+    syscall
+    cmp     rax, -1      ; 0 successful
+    jle     closeerror
+    mov     rdi, suces_close
+    call    printString
+    ret
+closeerror:
+    mov     rdi, error_close
+    call    printString
+    ret
+
+;----------------------------------------------------
+; rdi : file name
+deleteFile:
+    mov     rax, sys_unlink
+    syscall
+    cmp     rax, -1      ; 0 successful
+    jle     deleterror
+    mov     rdi, suces_delete
+    call    printString
+    ret
+deleterror:
+    mov     rdi, error_delete
+    call    printString
+    ret
+;----------------------------------------------------
+; rdi : file descriptor ; rsi: offset ; rdx : whence
+seekFile:
+    mov     rax, sys_lseek
+    syscall
+    cmp     rax, -1
+    jle     seekerror
+    mov     rdi, suces_seek
+    call    printString
+    ret
+seekerror:
+    mov     rdi, error_seek
+    call    printString
+    ret
+
+;----------------------------------------------------
---
+++ Common-code/sys-equal.asm
@@ -0,0 +1,48 @@
+
+%ifndef SYS_EQUAL
+%define SYS_EQUAL
+    sys_read     equ     0
+    sys_write    equ     1
+    sys_open     equ     2
+    sys_close    equ     3
+    
+    sys_lseek    equ     8
+    sys_create   equ     85
+    sys_unlink   equ     87
+      
+
+    sys_mmap     equ     9
+    sys_mumap    equ     11
+    sys_brk      equ     12
+    
+     
+    sys_exit     equ     60
+    
+    stdin        equ     0
+    stdout       equ     1
+    stderr       equ     3
+
+ 
+ 
+    PROT_READ     equ   0x1
+    PROT_WRITE    equ   0x2
+    MAP_PRIVATE   equ   0x2
+    MAP_ANONYMOUS equ   0x20
+    
+    ;access mode
+    O_RDONLY    equ     0q000000
+    O_WRONLY    equ     0q000001
+    O_RDWR      equ     0q000002
+    O_CREAT     equ     0q000100
+    O_APPEND    equ     0q002000
+
+    
+; create permission mode
+    sys_IRUSR     equ     0q400      ; user read permission
+    sys_IWUSR     equ     0q200      ; user write permission
+
+    NL            equ   0xA
+    Space         equ   0x20
+
+%endif
+
